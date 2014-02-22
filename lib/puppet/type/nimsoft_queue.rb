ab7b1a54c1a06f46a16b7f3aba8845a47af306ab
Stefan Schulte <stefan.schulte@taunusstein.net>
2014-02-23 00:25:13 +0100

    Add parameter documentation for nimsoft_queue

--- lib/puppet/type/nimsoft_queue.rb
+++ lib/puppet/type/nimsoft_queue.rb
@@ -14,19 +14,30 @@ Puppet::Type.newtype(:nimsoft_queue) do
 
   newparam(:name) do
     isnamevar
+    desc "The name of the queue"
   end
 
   ensurable
 
   newproperty(:active) do
+    desc "Specify whether this queue should be active or not. Valid values are `yes` and `no`"
     newvalues :yes, :no
   end
 
   newproperty(:type) do
+    desc "The type of the queue. A `post` queue sends messages directy to a destination hub. An
+      `attach` queue can be installed for other hubs to attach to and a `get` queue can be used
+      to get messages from a remote `attach` queue."
     newvalues :attach, :get, :post
   end
 
   newproperty(:subject, :array_matching => :all) do
+    desc "Specifying a subject for an attach queue or post queue defines that only messages with
+      that subject should be placed in that queue. Specifying a subject for a get queue is invalid"
+
+    validate do |value|
+      raise Puppet::Error, "subject must be provided as an array, not a comma-separated list." if value.include?(",")
+    end
   end
 
   newproperty(:remote_queue) do
@@ -40,6 +51,22 @@ Puppet::Type.newtype(:nimsoft_queue) do
   end
 
   newproperty(:bulk_size) do
+  end
+
+  validate do
+    if self[:address] and self[:address] != :absent and attach_queue?
+      raise Puppet::Error, "Specifying an address for an attach queue is invalid. Address is only valid for get queues and post queues"
+    end
+
+    if self[:remote_queue] and self[:remote_queue] != :absent and !get_queue?
+      raise Puppet::Error, "Specifying a remote queue is only valid for get queues"
+    end
+
+    if self[:subject] and get_queue?
+      raise Puppet::Error, "Specifying a subject is invalid for get queues"
+    end
+
+    true
   end
 
 end
